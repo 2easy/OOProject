db1b16c11b05fd4e9c73a27800c56303347efa60
Peter Olchawa <piotrusolchawa@gmail.com>
2010-06-06 16:50:41 +0200

    The judge class added.

--- src/game.rb
+++ src/game.rb
@@ -1,22 +1,22 @@
 #!/usr/bin/ruby
 
 require 'sdl'
+require 'system'
+require 'hud'
 require 'maze'
 require 'eatable'
-require 'sound_and_video'
 require 'characters'
+require 'sound_and_video'
 
 Key = Struct.new("Key",:left,:right,:up,:down)
+
 class Game
     def initialize mode
-        @event = SDL::Event.new
-        @level = 1
-        @key = Key.new
-        @players = []
-        @theme = SDL::Mixer::Wave.load("../sounds/theme.wav")
-        @death = SDL::Mixer::Wave.load("../sounds/death.wav")
-        @new_level = SDL::Mixer::Wave.load("../sounds/newlevel.wav")
-        @chomp_a_ghost = SDL::Mixer::Wave.load("../sounds/chompaghost.wav")
+        @event      = SDL::Event.new
+        @key        = Key.new
+        @level      = 1
+        @players    = []
+        @theme      = SDL::Mixer::Wave.load("../sounds/theme.wav")
         case mode
             when :single_player
                 @pacman = Character::PacMan.new(:pacman)
@@ -28,87 +28,40 @@ class Game
                 @maze = Maze.new
                 @eatable = ToEat::Eatable.new
         end
+        @judge = System::Judge.new(self,@maze,@players,@ghosts)
     end
     def draw
         @maze.redraw
         @eatable.draw(@maze.eatable,@maze)
-        for name in @ghosts
-            name.draw
-        end
-        @pacman.draw 
+        @ghosts.each { |ghost| ghost.draw }
+        @players.each { |player| player.draw }
+        HUD::draw
         Video::Game_screen.flip
     end
-    def judge
-        if @maze.all_dots_eaten?
-            @players.each { |player| player.set_defaults }
-            @ghosts.each { |ghost| ghost.set_defaults }
-            @maze.set_defaults
-            self.draw
-            SDL::Mixer.play_channel(Sound::Background_channel,
-                                    @new_level,0)
-            sleep 1
-            return
-        end
-        for ghost in @ghosts
-            for player in @players
-                player.check_power_time
-                if self.collide?(ghost,player)
-                    case ghost.state
-                        when :weak 
-                            ghost.state = :dead
-                            SDL::Mixer.play_channel(Sound::Pacman_channel,
-                                                    @chomp_a_ghost,0)
-                            sleep 0.5
-                        when :flashing then
-                            ghost.state = :dead
-                            SDL::Mixer.play_channel(Sound::Pacman_channel,
-                                                    @chomp_a_ghost,0)
-                            sleep 0.5
-                        when :alive then
-                            self.new_round 
-                        else next
-                    end
-                end
-            end
-        end 
+    def draw_get_ready
+        Font::My_font.draw_solid_utf8(Video::Game_screen,
+                                      "GET READY!",
+                                      12*Video::Image_width-10,
+                                      17*Video::Image_height,
+                                      *Video::White_color)
+        Video::Game_screen.flip
     end
-    def collide? ghost,player
-        return true if ((ghost.sprite_x - player.sprite_x).abs < 15 and
-                        (ghost.sprite_y - player.sprite_y).abs < 15)
-        return false
-    end
-    def new_round
-        for name in @players
-            name.change_state_to(:dead)
-            name.subtract_life
-            SDL::Mixer.play_channel(Sound::Background_channel,@death,0)
-            self.death_animation
-            name.set_defaults
-        end
-        for name in @ghosts
-            name.set_defaults
-        end
-        sleep 1
-    end
-    def death_animation
-        for i in 0...25
-            self.draw 
-            sleep 0.05
-        end
-    end
-
     def run
         direction = :left
         @maze.draw
+        self.draw 
+        self.draw_get_ready
         SDL::Mixer.play_channel(Sound::Background_channel,@theme,0)
+        sleep Sound::Theme_length
         while @pacman.alive?
             self.draw 
+
             @pacman.move(direction,@maze,@ghosts)
             for name in @ghosts
                 name.recover(@maze,@pacman)
                 name.move(@maze,@pacman)
             end
-            self.judge
+            @judge.check_all
 
             if @event.poll != 0 then
                 if @event.type == SDL::Event::QUIT then
--- src/sound_and_video.rb
+++ src/sound_and_video.rb
@@ -7,6 +7,7 @@ module Video
     Image_width    = 25
     Image_height   = 25
     Init_animation = -1
+    White_color    = [255,255,255]
 
     begin
         SDL.init(SDL::INIT_VIDEO)
@@ -32,7 +33,8 @@ module Sound
     Background_channel = 0
     Pacman_channel = 1
     Ghosts_channel = 2 
-
+    Theme_length   = 4.5
+    Chomp_a_ghost_length = 0.5
     begin
         SDL.init(SDL::INIT_AUDIO)
         SDL::Mixer.open(frequency=22050,format=SDL::Mixer::DEFAULT_FORMAT,cannels=SDL::Mixer::DEFAULT_CHANNELS,chunksize=4096)
@@ -40,4 +42,17 @@ module Sound
         puts "Failed to initialize sound. Error message: #{a}"
         SDL.quit
     end 
+end
+
+module Font
+    begin
+        SDL::TTF.init
+        My_font = SDL::TTF.open("../images/arial.ttf",25) 
+        My_font.style = SDL::TTF::STYLE_BOLD #STYLE_NORMAL
+        HUD_font = SDL::TTF.open("../images/arial.ttf",25) 
+        HUD_font.style = SDL::TTF::STYLE_BOLD #STYLE_NORMAL
+    rescue Exception => a 
+        puts "Failed to initialize font. Error message: #{a}"
+        SDL.quit
+    end
 end
