bea17265228785935d00af5cef6f13e09c654ff4
Peter Olchawa <piotrusolchawa@gmail.com>
2010-06-10 11:18:40 +0200

    hud refacored

--- src/hud.rb
+++ src/hud.rb
@@ -1,41 +1,25 @@
 module HUD
     def HUD::draw
-        Video::Game_screen.fill_rect(30*Video::Image_width,
-                                     1*Video::Image_height,
-                                     4*Video::Image_width,
-                                     6*Video::Image_height,
-                                     Video::Black_color)    
-        Font::HUD_font.draw_solid_utf8(Video::Game_screen,
-                                       "Score:",
-                                       30*Video::Image_width,
-                                       1*Video::Image_height,
-                                       *Video::White_color)
-        Font::HUD_font.draw_solid_utf8(Video::Game_screen,
-                                       "#{$SCORE}",
-                                       30*Video::Image_width,
-                                       2*Video::Image_height,
-                                       *Video::White_color)
-        Font::HUD_font.draw_solid_utf8(Video::Game_screen,
-                                      "Lifes:",
-                                      30*Video::Image_width,
-                                      3*Video::Image_height,
-                                      *Video::White_color)
-        Font::HUD_font.draw_solid_utf8(Video::Game_screen,
-                                      "Level:",
-                                      30*Video::Image_width,
-                                      5*Video::Image_height,
-                                      *Video::White_color)
-        Font::HUD_font.draw_solid_utf8(Video::Game_screen,
-                                      "#{$LEVEL}",
-                                      30*Video::Image_width,
-                                      6*Video::Image_height,
-                                      *Video::White_color)
+        blacken_rect 30,1,4,6
+        write "Score:",30,1,Video::White_color
+        write "#{$SCORE}",30,2,Video::White_color
+        write "Lifes:",30,3,Video::White_color
+        write "Level:",30,5,Video::White_color
+        write "#{$LEVEL}",30,6,Video::White_color
         Video::Game_screen.flip
     end
-    def update pacman,score,level
-        SDL::Screen.blit(@my_pic[@direction],
-                    pict_x*Video::Image_width, 0,
-                    Video::Image_width,Video::Image_height,
-                    Video::Game_screen,@sprite_coords[:x],@sprite_coords[:y])
+    def HUD::blacken_rect x,y,width,height
+        Video::Game_screen.fill_rect(x*Video::Image_width,
+                                     y*Video::Image_height,
+                                     width*Video::Image_width,
+                                     height*Video::Image_height,
+                                     Video::Black_color)    
+    end
+    def HUD::write text,x,y,colour
+        Font::HUD_font.draw_solid_utf8(Video::Game_screen,
+                                      text,
+                                      x*Video::Image_width,
+                                      y*Video::Image_height,
+                                      *colour)
     end
 end
